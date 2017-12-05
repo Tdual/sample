f6ae322d047107dd893b199e20a91bf71c171db3
tamoto.yoshifumi <tamoto.yoshifumi@05-17-072TMTYSF.local>
2017-12-05 18:36:55 +0900

    add t-SNE

---
+++ D3js/scatter.js
@@ -0,0 +1,72 @@
+var width= screen.width;
+var height = screen.height;
+
+var x = d3.scale.linear()
+    .range([0, width]).nice();
+
+var y = d3.scale.linear()
+    .range([height, 0]).nice()
+
+
+
+
+d3.csv("vec.csv", function(data) {
+  data.forEach(function(d) { 
+       d.x = +d.x;
+       d.y = +d.y;
+  });
+
+  var xMax = d3.max(data, function(d) { return d.x; }) * 1.05,
+       xMin = d3.min(data, function(d) { return d.x; }),
+       xMin = xMin > 0 ? 0 : xMin,
+       yMax = d3.max(data, function(d) { return d.y; }) * 1.05,
+       yMin = d3.min(data, function(d) { return d.y; }),
+       yMin = yMin > 0 ? 0 : yMin;
+
+  x.domain([xMin, xMax]);
+  y.domain([yMin, yMax]);
+
+  var zoomBeh = d3.behavior.zoom()
+      .x(x)
+      .y(y)
+      .scaleExtent([0, 500])
+      .on("zoom", zoom);
+
+  var svg = d3.select("#scatter")
+    .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .call(zoomBeh);
+
+  var objects = svg.append("svg")
+      .classed("objects", true)
+      .attr("width", width)
+      .attr("height", height);
+
+  objects.selectAll(".dot")
+      .data(data)
+      .enter()
+      .append("svg:image")
+      .attr('x',-9)
+      .attr('y',-12)
+      .attr('width', 20)
+      .attr('height', 20)
+      .classed("dot", true)
+      .attr("transform", transform)
+      .attr("xlink:href", function(d){ return "https://pbs.twimg.com/profile_images/765535129435373569/jaTQWoSq_bigger.jpg"; });
+ 
+
+  function zoom() {
+    svg.selectAll(".dot")
+        .attr("transform", transform);
+  }
+
+  function transform(d) {
+    if(d.x && d.y){
+        return "translate(" + x(d.x) + "," + y(d.y) + ")";
+    }
+  }
+ 
+});
+
